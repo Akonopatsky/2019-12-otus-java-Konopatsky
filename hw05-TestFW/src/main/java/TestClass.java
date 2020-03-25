ef778840ba61a70dbed826880b02683af7bbd917
Akonopatsky <Akonopatsky@gmail.com>
2020-03-26 00:11:14 +0400

    версия 1.1

--- hw05-TestFW/src/main/java/TestClass.java
+++ hw05-TestFW/src/main/java/TestClass.java
@@ -1,44 +1,47 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.otus.testFW.runing.After;
 import ru.otus.testFW.runing.Before;
 import ru.otus.testFW.runing.Test;
 
 public class TestClass {
+    static private Logger logger = LoggerFactory.getLogger(TestClass.class);
 
     @Before
     public void methodBefore() {
-        System.out.println("before ");
+        logger.info("before ");
     }
 
     @Test
     public void test1() {
-        System.out.println("test1 ");
+        logger.info("test1 ");
     }
 
     @Test
     public void test2() {
-        System.out.println("test2 ");
+        logger.info("test2 ");
         throw new IndexOutOfBoundsException();
     }
 
     @Test
     public void test4() {
-        System.out.println("test4 ");
+        logger.info("test4 ");
         throw new UnsupportedOperationException();
     }
 
     @Test
     public void test3() {
-        System.out.println("test3 ");
+        logger.info("test3 ");
     }
 
     @After
     public void methodAfter1() {
-        System.out.println("methodAfter1 ");
+        logger.info("methodAfter1 ");
     }
 
     @After
     public void methodAfter2() {
-        System.out.println("methodAfter2 ");
+        logger.info("methodAfter2 ");
     }
 
 }
