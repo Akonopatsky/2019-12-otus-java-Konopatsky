372bbc4bf548b3734f6dac0efac6d91a48ba603e
Akonopatsky <Akonopatsky@gmail.com>
2020-08-22 19:00:55 +0300

    mvp но не универсально

--- hw15-multithreading/src/main/java/ru/otus/hw15/DigitSequence.java
+++ hw15-multithreading/src/main/java/ru/otus/hw15/DigitSequence.java
@@ -7,41 +7,32 @@ public class DigitSequence {
     public static final Logger logger = LoggerFactory.getLogger(DigitSequence.class);
     public boolean isOddStep = true;
 
-
     public void setOddStep(boolean oddStep) {
         isOddStep = oddStep;
     }
 
     private synchronized void action() {
         boolean isOddThread = isOddStep;
-        logger.info("set isOddThread: {}", isOddThread);
-        Thread.currentThread().setName(isOddThread ? "first" : " second");
+        Thread.currentThread().setName(isOddThread ? "first" : "second");
         checkStep(isOddThread);
-        notifyAll();
-        for (int i = 1; i <= 10; i++) {
-            logger.info("{}: {}", isOddThread ? "first" : " second", i);
-            checkStep(isOddThread);
 
+        for (int i = 1; i <= 10; i++) {
+            checkStep(isOddThread);
+            logger.info("{}", i);
         }
         for (int i = 9; i > 0; i--) {
-            logger.info("{}", i);
             checkStep(isOddThread);
+            logger.info("{}", i);
         }
-        logger.debug("{} end", Thread.currentThread().getName());
-
     }
 
     private void checkStep(boolean isOddThread) {
-        setOddStep(!isOddThread);
-        notifyAll();
-
         try {
-            logger.debug("меняем поток isOddThread: {} isOddStep: {}", isOddThread, isOddStep);
             while (isOddThread != isOddStep) {
-                logger.debug("wait", isOddThread);
                 this.wait();
             }
-
+            setOddStep(!isOddThread);
+            notifyAll();
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
@@ -51,7 +42,6 @@ public class DigitSequence {
         DigitSequence digitSequence = new DigitSequence();
         new Thread(() -> digitSequence.action()).start();
         new Thread(() -> digitSequence.action()).start();
-
     }
 
     private static class myInterruptedException extends RuntimeException {
