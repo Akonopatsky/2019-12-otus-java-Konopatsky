a88c23f519b90f1cefc81878e6cde3409b198ecd
Longrad <kavis@mail.ru>
2020-09-26 12:18:47 +0400

    перенос

--- hw17-MessageServer/Frontend/src/main/java/ru/otus/hw17/frontend/socket/MSSocketClientImpl.java
+++ hw17-MessageServer/Frontend/src/main/java/ru/otus/hw17/frontend/socket/MSSocketClientImpl.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import ru.otus.hw17.frontend.messagesystem.message.Message;
 
 public class MSSocketClientImpl implements MSSocketClient {
+    private static final int PORT = 8090;
+    private static final String HOST = "localhost";
     private static final Logger logger = LoggerFactory.getLogger(MSSocketClientImpl.class);
 
 
